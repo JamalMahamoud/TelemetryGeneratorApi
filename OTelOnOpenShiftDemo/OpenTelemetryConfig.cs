248b203c194dcacfa8cab8d6da1eaf9580856682
jamal <jamalm35@gmail.com>
2023-06-26 12:06:32 +0100

    adding more

--- OTelOnOpenShiftDemo/OpenTelemetryConfig.cs
+++ OTelOnOpenShiftDemo/OpenTelemetryConfig.cs
@@ -5,8 +5,9 @@ namespace OptelDataGenerator;
 public static class OpenTelemetryConfig
 {
     public const string ServiceName = "Microlise.OTelOnOpenShiftDemo.Service";
+
     //will be use to link OTel with .Net counter
     public const string MeterName = "OTelOnOpenShiftDemo.Meter";
-    
+
     public static ActivitySource ActivitySource = new(ServiceName);
 }
